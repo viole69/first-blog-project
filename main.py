67cfd66ac8a25012f138c0fe162d936212e49bfd
Vansh <bigvhomie@gmail.com>
2024-12-01 20:04:51 +0530

    initial commit

--- main.py
+++ main.py
@@ -15,10 +15,12 @@ from wtforms.validators import DataRequired, email
 from forms import CommentForm
 
 from forms import CreatePostForm
+import os
+from dotenv import load_dotenv
 
-
+load_dotenv()
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] =  os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -43,7 +45,7 @@ class LoginForm(FlaskForm):
 
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
